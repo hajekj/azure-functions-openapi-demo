a8e102360aaada2a568111d6c4c582bb9fbef517
Jan Hajek <hajek.j@hotmail.com>
2021-04-22 15:39:31 +0200

    Add sample with OpenAPI 2.0 result.

--- AzureFunctionsOpenAPIDemo/Function1.cs
+++ AzureFunctionsOpenAPIDemo/Function1.cs
@@ -76,5 +76,34 @@ namespace AzureFunctionsOpenAPIDemo
                 Content = new StringContent(openApiDocuments.First().Value.SerializeAsJson(OpenApiSpecVersion.OpenApi3_0), Encoding.UTF8, "application/json")
             };
         }
+
+        [FunctionName("SwaggerV2")]
+        public static async Task<HttpResponseMessage> RunSwagger2([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req, ILogger log)
+        {
+            var input = new OpenApiGeneratorConfig(
+                annotationXmlDocuments: new List<XDocument>()
+                {
+                    XDocument.Load(@"AzureFunctionsOpenAPIDemo.xml"),
+                },
+                assemblyPaths: new List<string>()
+                {
+                    @"bin\AzureFunctionsOpenAPIDemo.dll"
+                },
+                openApiDocumentVersion: "V1",
+                filterSetVersion: FilterSetVersion.V1
+            );
+            input.OpenApiInfoDescription = "This is a sample description...";
+
+            var generator = new OpenApiGenerator();
+            var openApiDocuments = generator.GenerateDocuments(
+                openApiGeneratorConfig: input,
+                generationDiagnostic: out GenerationDiagnostic result
+            );
+
+            return new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(openApiDocuments.First().Value.SerializeAsJson(OpenApiSpecVersion.OpenApi2_0), Encoding.UTF8, "application/json")
+            };
+        }
     }
 }
